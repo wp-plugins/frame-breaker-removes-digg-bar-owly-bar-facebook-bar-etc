e9c0a77c8be5ac0c30caf7e870e719810ab707a0
pbhj <pbhj@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-11 00:52:47 +0000

    Adding first versino of frame-breaker plugin
    git-svn-id: https://plugins.svn.wordpress.org/frame-breaker-removes-digg-bar-owly-bar-facebook-bar-etc/trunk@109573 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ frame-breaker.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Plugin Name: Die Digg-bar die!
+Plugin URI: http://alicious.com/2009/remove-digg-bar-from-website/
+Description: You want people to visit your website rather than be stuck in a frame or iframe, you need this plugin.
+Version: 0.2
+Author: pbhj
+Author URI: http://alicious.com
+*/
+add_action('wp_head', 'frameBreaker');
+function frameBreaker() { 
+  // frame breaker code for , eg, Digg, Google Images, Ow.ly, Facebook
+  // if the server name is not in the referer address field
+  // serve the javascript for frame breaking
+  if (!stristr($_SERVER['HTTP_REFERER'], $_SERVER['SERVER_NAME'])) { ?>
+    
+<script language="javascript" type="text/javascript">
+//<!--
+//<![CDATA[
+window.onload = function() {
+  if (top.location != location) {
+    top.location.href = document.location.href;
+  }
+}
+// ]]>
+// -->
+</script>
+<?php
+  }
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,23 @@
+=== Admin Links Widget ===
+Contributors: pbhj
+Version: 0.2
+Tags: digg bar,frames,iframe,jailbreak,break,remove
+Requires at least: 1.5
+Tested up to: 2.5.1
+Stable Tag: 0.2
+
+== Description ==
+You want people to visit your website rather than be stuck in a frame or iframe, you need this plugin.
+
+== Setup Instructions ==
+1. Extract frame-breaker.php  into your wp-content/plugins folder (or a subfolder)
+2. Activate the plugin in Wordpress
+3. Your done, drop by my website (alicious.com) and let me know any feedback.
+
+== Release History ==
+
+0.2 -- fixed erroneous character
+0.1 -- Initial release
+
+== Feedback ==
+pbhj /|7 alicious D07 com
